0bf0f24b34779e7744d987087dfe4d4395f0de51
Suntaz <suntaz.wu@mitake.com.tw>
2021-08-06 14:55:38 +0800

    add:project init

---
+++ src/test/java/tw/com/mitake/SpringProject2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.mitake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
