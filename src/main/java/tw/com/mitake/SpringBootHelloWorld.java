c3cc7432cc3252af9edf79159b98c7268bff234c
Suntaz <suntaz.wu@mitake.com.tw>
2021-08-10 09:01:59 +0800

    add: a simple controller

---
+++ src/main/java/tw/com/mitake/SpringBootHelloWorld.java
@@ -0,0 +1,20 @@
+package tw.com.mitake;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringBootHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "hello";
+	}
+}
+
+
